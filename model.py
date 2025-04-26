d8b5779ba0d303eb20e2aa94be5e8d7cd048d272
Your Name <you@example.com>
2025-04-26 18:13:20 +0200

    implementing main functions and pre processing

---
+++ model.py
@@ -0,0 +1,95 @@
+import numpy as np
+
+#! hyperparameters
+class Config:
+    vocab_size = 32768
+    ctx_length = 512
+    n_layers = 4
+    d_model = 128
+
+#! GPT functions
+class Linear:
+    def __init__(self, features_in: int, features_out: int, bias: bool=True):
+        k = 1 / np.sqrt(features_in)
+
+        self.w = np.random.uniform(low=-k, high=k, size=(features_in, features_out))
+        self.b = np.random.uniform(low=-k, high=k, size=(features_out))
+
+        self.bias = bias
+
+    def __call__(self, x):
+        return x @ self.w + self.b if self.bias else x @ self.w
+
+class Embedding:
+    def __init__(self, num_embeddings: int, embedding_dim: int):
+        self.w = np.random.normal(loc=0, scale=1, size=(num_embeddings, embedding_dim))
+
+        self.embedding_dim = embedding_dim
+
+    def __call__(self, x):
+        B, T = x.shape
+
+        new_x = np.zeros((B, T, self.embedding_dim))
+
+        for i in range(B):
+            for j in range(T):
+                new_x[i, j] = self.w[x[i, j]]
+
+        return np.array(new_x)
+    
+class LayerNorm:
+    def __init__(self, *normalized_shape: list, eps: int=1e-5, bias: bool=True):
+        self.w = np.ones((normalized_shape))
+        self.b = np.zeros((normalized_shape))
+
+        self.eps = eps
+        self.bias = bias
+    def __call__(self, x):
+        _, T, _ = x.shape
+
+        mean = np.mean(x, axis=-1, keepdims=True)
+        var = np.var(x, axis=-1, keepdims=True)
+        norm = (x - mean) / np.sqrt(var + self.eps) * self.w + self.b
+
+        return norm
+
+#! model
+class PreProcessing(Config):
+    def __init__(self):
+        self.embedding = Embedding(self.vocab_size, self.d_model)
+        self.pos_encoding = Embedding(self.ctx_length, self.d_model)
+
+    def __call__(self, tokens):
+        emb = self.embedding(tokens) # (B, T, C)
+        pe = self.pos_encoding(np.arange(0, T).reshape(1, -1)) # (1, T, C)
+
+        x = emb + pe # (B, T, C)
+
+        return x
+
+class Block:
+    def __init__(self):
+        pass
+
+class PostProcessing:
+    def __init__(self):
+        pass
+
+class GPT(Config):
+    def __init__(self):
+        super().__init__()
+
+        self.pre_processing = PreProcessing()                  
+        self.blocks = [Block() for _ in range(self.n_layers)]
+        self.post_processing = PostProcessing()
+
+    def __call__(self, tokens):
+        x = self.pre_processing(tokens) # (B, T) -> (B, T, C)
+
+        return x
+
+model = GPT()
+
+B, T = 4, 256
+tokens = np.ones((B, T), dtype=np.int16)
+print(model(tokens))
